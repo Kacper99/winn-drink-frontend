f5dd91e01d29779eb4d639a50d8ea3986f741522
Kacper Martela <kacper99@hotmail.co.uk>
2019-09-02 11:15:52 +0100

    Add NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
+
+export default function () {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Brand href="#home">Winn Drink</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href="#features">Add Players</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
