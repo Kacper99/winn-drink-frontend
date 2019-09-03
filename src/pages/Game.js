26ef420471c4e645750d382236dc752877785ce7
Kacper Martela <kacper99@hotmail.co.uk>
2019-09-03 11:59:55 +0100

    Add support for follow up cards

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -17,7 +17,7 @@ class Game extends React.Component {
         //192.168.1.156
         const response = await fetch("http://192.168.1.156:5000/cards", {mode: "cors", method: "GET"});
         const myJson = await response.json();
-        const question = myJson.pop();
+        const question = {type: "Begin game", text: ""};
         console.log(myJson);
         console.log(question);
         this.setState({questions: myJson, question: question})
@@ -28,25 +28,38 @@ class Game extends React.Component {
             this.setState({endgame: true});
         } else {
             const randomQuestion = this.state.questions.pop();
-            console.log(randomQuestion);
-            this.setState({question: randomQuestion})
+            console.log(randomQuestion)
+            let question = randomQuestion.text;
+            let playerPool = this.props.players.slice(); //Clone the array
+            const regex = /\$NAME/;
+            const globalRegex = /\$NAME/g;
+            var count = (question.match(globalRegex) || []).length; //Get number of names in the card
+
+            let followUpCard = randomQuestion.followUp; //Try to get a follow up card (CAN BE UNDEFINED)
+            console.log(followUpCard);
+
+            for (let i = 0; i < count; i++) {
+                const randomIndex = Math.floor(Math.random() * playerPool.length); //Get a random index to choose a player from
+                const randomPlayer = playerPool[randomIndex];
+                playerPool.splice(randomIndex, 1);
+                question = question.replace(regex, randomPlayer)
+
+                if (followUpCard !== undefined) { //If there is a follow up card replace the name
+                    followUpCard.text = followUpCard.text.replace(regex, randomPlayer);
+                }
+            }
+
+            if (followUpCard !== undefined) { //If there is a follow up card add it to the stack
+                console.log(this.state.questions.length);
+                console.log(this.state.questions);
+                this.state.questions.splice(0, 0, followUpCard); //TODO: Randomly set the number of rounds
+            }
+
+            this.setState({question: {type: randomQuestion.type, text: question}})
         }
     }
 
     render() {
-        let playerPool = this.props.players.slice(); //Clone the array
-        const regex = /\$NAME/;
-        const globalRegex = /\$NAME/g;
-        let question = this.state.question.text;
-        var count = (question.match(globalRegex) || []).length; //Get number of names in the card
-
-        for (let i = 0; i < count; i++) {
-            const randomIndex = Math.floor(Math.random() * playerPool.length); //Get a random index to choose a player from
-            const randomPlayer = playerPool[randomIndex];
-            playerPool.splice(randomIndex, 1);
-            question = question.replace(regex, randomPlayer)
-        }
-
         return (
             <div className="container">
                 <div className="row">
@@ -61,7 +74,7 @@ class Game extends React.Component {
                             <span>
                                 <span className="display-3">{this.state.question.type}</span>
                                 <br/>
-                                <span className="h1 game-text">{question}</span>
+                                <span className="h1 game-text">{this.state.question.text}</span>
                             </span>
                         }
                     </div>
